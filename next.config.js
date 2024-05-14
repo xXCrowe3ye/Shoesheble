8676e4b6f8b691268ab207e53b677b9222ae0ae0
xXCrowe3ye <luffy1356@gmail.com>
2024-05-15 04:07:13 +0800

    VV

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const withPWA = require("next-pwa")({
-
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
   dest : 'public'
 });
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    runtimeCaching,
-  },
+
   images: {
     domains: ["fakestoreapi.com","m.media-amazon.com","static.nike.com"],
   },
