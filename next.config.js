67650346ac509d5d56dd7eda37de0017e7519e31
xXCrowe3ye <luffy1356@gmail.com>
2024-05-15 03:46:26 +0800

    lol

--- next.config.js
+++ next.config.js
@@ -1,19 +1,13 @@
-const withPWA = require("next-pwa")({
-  runtimeCaching,
-
-  dest : 'public',
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-
-
-
-});
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
-
+  dest: 'public',
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  runtimeCaching,
   images: {
-    domains: ["fakestoreapi.com","m.media-amazon.com","static.nike.com"],
+    domains: ["fakestoreapi.com", "m.media-amazon.com", "static.nike.com"],
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
