301e5a96340fe226a424f66f4f898ddab78dceee
xXCrowe3ye <luffy1356@gmail.com>
2024-05-15 04:09:04 +0800

    AA

--- next.config.js
+++ next.config.js
@@ -1,12 +1,28 @@
+// const withPWA = require("next-pwa")({
+//   runtimeCaching,
+//   disable: process.env.NODE_ENV === "development",
+//   dest : 'public'
+// });
+// const runtimeCaching = require('next-pwa/cache')
+
+// module.exports = withPWA({
+
+//   images: {
+//     domains: ["fakestoreapi.com","m.media-amazon.com","static.nike.com"],
+//   },
+//   env: {
+//     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+//   },
+// });
+const runtimeCaching = require('next-pwa/cache');
 const withPWA = require("next-pwa")({
-  runtimeCaching,
+  dest : 'public',
   disable: process.env.NODE_ENV === "development",
-  dest : 'public'
+  register: true,
+  runtimeCaching,
 });
-const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-
   images: {
     domains: ["fakestoreapi.com","m.media-amazon.com","static.nike.com"],
   },
